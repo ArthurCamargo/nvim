f36c1572e6d689cc1f0f1056da94d7815778e0bb
Arthur Camargo <avccamargo@inf.ufrgs.br>
2025-08-06 16:35:24 -0300

    fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,12 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function ()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find_files' })
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find_files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live_grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help_tags' })
+        vim.keymap.set('n', '<leader>fi', builtin.lsp_implementations , { desc = 'Telescope lsp_implementation'})
         vim.keymap.set('n', '<leader>ft', "<cmd> Telescope thesaurus lookup", { desc = 'Telescope help tags' })
     end
     },
